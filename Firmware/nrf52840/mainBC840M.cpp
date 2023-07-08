7a45c1c25af6da8e209c1c065492c5041e4676c5
Joshua Brewster (Bubba Skrimp) <brewster.joshua1@gmail.com>
2023-07-08 15:26:50 -0700

    fix

--- Firmware/nrf52840/mainBC840M.cpp
+++ Firmware/nrf52840/mainBC840M.cpp
@@ -17,17 +17,17 @@
 
 //FIX: for gpio 1.xx, use either 100 + the .xx or 32 + .xx (like the overlay)
 
-#define ADS_CS              ((uint8_t)33)  //   47 (BC840M),   33 (BT840)
-#define DATA_READY_GPIO     ((uint8_t)6)  //   37 (BC840M),    6 (BT840)
-#define ADS_RESET           ((uint8_t)8)  //   45 (BC840M),    8 (BC840M)
+#define ADS_CS              ((uint8_t)47)  //   47 (BC840M),   33 (BT840)
+#define DATA_READY_GPIO     ((uint8_t)37)  //   37 (BC840M),    6 (BT840)
+#define ADS_RESET           ((uint8_t)45)  //   45 (BC840M),    8 (BC840M)
 
-#define ADS_1_CS            ((uint8_t)36)  //  42 (BC840M),    36 (BT840)
-#define DATA_READY_1_GPIO   ((uint8_t)40)  //  24 (BC840M),    40 (BT840)
+#define ADS_1_CS            ((uint8_t)42)  //  42 (BC840M),    36 (BT840)
+#define DATA_READY_1_GPIO   ((uint8_t)24)  //  24 (BC840M),    40 (BT840)
 #define ADS_1_RESET         ((uint8_t)29)  //  29 (BC840M),    35 (BT840)
 
-#define DBG_LED             ((uint8_t)25)  //  16 (BC840M),    25 (BT840)     
-#define MAX_INT             ((uint8_t)7)  //  28 (BC840M),     7 (BT840)
-#define MPU_INT             ((uint8_t)38)   //  2 (BC840M),     38 (BT840)
+#define DBG_LED             ((uint8_t)16)  //  16 (BC840M),    25 (BT840)     
+#define MAX_INT             ((uint8_t)28)  //  28 (BC840M),     7 (BT840)
+#define MPU_INT             ((uint8_t)2)   //  2 (BC840M),     38 (BT840)
 
 
 #define PWM_CLK         ((uint32_t)8192000) //Frequency (Hz)
@@ -43,7 +43,7 @@ static const uint8_t nLEDs = 3;
 
 //list the GPIO in the order we want to flash. 255 is ambient
 static uint8_t LED_gpio[nLEDs] = { 
-    20, 21, 255//, 15, 25, 
+    20, 26, 255//, 15, 25, 
     //15, 25, 15, 25, 
     //15, 25, 15, 25, 
     //15, 25, 15, 25, 
